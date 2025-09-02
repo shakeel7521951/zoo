8c7b440bd79349e861140f01dbbd2e4af301fdbc
1216isha <ishanaveed40@gmail.com>
2025-09-02 15:26:12 +0500

    Services Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,26 @@
+import React, { useState } from "react";
 import "./App.css";
 import Footer from "./components/common/Footer";
 import Navbar from "./components/common/Navbar";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+import Services from "./pages/Services";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import CartModal from "./components/common/CartModal"; // <-- import modal
 
 const MainFunction = () => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  const openCart = () => setIsCartOpen(true);
+  const closeCart = () => setIsCartOpen(false);
+
   return (
     <div>
-      <Navbar />
+      <Navbar openCart={openCart} />   {/* pass openCart function */}
       <Outlet />
       <Footer />
+      {isCartOpen && <CartModal close={closeCart} />}   {/* render modal */}
     </div>
   );
 };
@@ -17,12 +28,19 @@ const MainFunction = () => {
 const router = createBrowserRouter([
   {
     element: <MainFunction />,
-    children: [{ path: "/", element: <Home /> }],
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/services", element: <Services /> }
+    ],
   },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 }
 
 export default App;
